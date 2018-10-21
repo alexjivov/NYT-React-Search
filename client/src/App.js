d1e439c403bc34e93bf1caef522fbe2362c41d8d
Alex Jivov <alexjivov@gwu.edu>
2018-10-21 18:57:39 -0400

    app file and components folders added

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 
-import './App.css';
+//COMPONENTS
+import Home from './pages/Home/Home';
+import Saved from './pages/Saved/Saved';
+
+// import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
+      <div>
+        <Router>
+          <switch>
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/Saved" component={Saved}/>
+          </switch>
+        </Router>
       </div>
     );
   }
